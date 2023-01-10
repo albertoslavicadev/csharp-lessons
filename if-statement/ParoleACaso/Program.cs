4d22e1b6640cbe0c57db464b69af56113470ac3f
albertoslavicadev <albertoslavica19@gmail.com>
2023-01-10 15:15:41 +0100

    If-statements

---
+++ if-statement/ParoleACaso/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParoleACaso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+            bool isTall = false;
+            if (isMale && isTall) //specify a conditon inside the if statement  // && AND(both operators need to be true)
+            {                                                                   // || OR(just one operator need to be true)
+                Console.WriteLine("You are a tall male");
+            }
+            else if (isMale && !isTall) // ! means that the bool is false
+            {
+                Console.WriteLine("You are a short male");
+            } 
+            else if (!isMale && isTall)
+            {
+                Console.WriteLine("You are a tall female");
+            } 
+            else
+            {
+                Console.WriteLine("You are a short female");
+            }
+            Console.ReadLine();
+        }
+    }
+}
