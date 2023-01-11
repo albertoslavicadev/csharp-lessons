e21b8dec932fb01979b6f851a6e2be63b3c14353
albertoslavicadev <albertoslavica19@gmail.com>
2023-01-11 10:20:57 +0100

    New Lessons

---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWorld = "lion";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            do
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter your guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+            while (guess != secretWorld && !outOfGuesses);
+
+            if (outOfGuesses)
+            {
+                Console.WriteLine("You lose");
+            } else
+            {
+                Console.WriteLine("You win! You guessed the correct word in " + guessCount + " try");
+            }
+            Console.ReadLine();
+        }
+    }
+}
